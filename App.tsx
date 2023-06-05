767a5e01e17a936fb9d384b72d62c34ebc917c2a
JeremyDevCode <jeremydevcode@gmail.com>
2023-06-05 10:50:22 -0500

    feat: add Main

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableWithoutFeedback onPress={() => alert("Hemos tocado el texto!")}>
+        <Text style={styles.text}>Testing React Native!</Text>
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#0a0a0a",
+    color: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#e3e6e8",
   },
 });
